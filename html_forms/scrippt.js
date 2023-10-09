5cd42f1f6eb3b3a4acb01b45358267026cd33362
Richard-WanderaM <richardwandera2023@gmail.com>
2023-10-09 22:45:28 +0300

    js file for logic

---
+++ html_forms/scrippt.js
@@ -0,0 +1,8 @@
+function submitForm(event) {
+    event.preventDefault();
+  
+    // Let me add form submission logic here
+    // For simplicity, let's just log a message
+    console.log('Form submitted!');
+  }
+  
